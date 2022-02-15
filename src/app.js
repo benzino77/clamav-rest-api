8d93172c310e973edc5be1f19c4f5fe64be6ac4a
Gavin Albantow <16983537+galbantow@users.noreply.github.com>
2022-02-16 08:41:42 +1100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,25 @@ const { makeServer } = require('./server');
 
 (async () => {
   try {
-    await makeServer(config);
+    var startupRetry = config.avConfig.clamdscan.startupRetry;
+    var count = 0;
+    var success = false;
+    while (count != startupRetry) {
+      count = count + 1;
+      try {
+        await makeServer(config);
+        success = true;
+        break;// Server is online.
+      }
+      catch (error) {
+        console.log(`"(${count}-${startupRetry}): Clam AV Server not ready!!"`);
+        console.log(error);
+        await new Promise(r => setTimeout(r, 5000));
+      }
+    }
+    if (success == false) {
+      throw new Error("Application failed to start. Please check the message log.");
+    }
   } catch (e) {
     console.log(e.message);
   }
