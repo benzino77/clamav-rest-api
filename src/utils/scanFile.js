60dffd377ecbb07030180f8d07046f41850b0416
Evil.2000 <evil.zweitausend@gmail.com>
2023-08-07 18:54:04 +0200

    Replaced scanStream with passthrough for stability.

--- src/utils/scanFile.js
+++ src/utils/scanFile.js
@@ -1,10 +1,30 @@
 const { Readable } = require('stream');
 
 const scanFile = async (upload, av) => {
+  if (upload.size <= 0) {
+    return {
+      name: upload.name,
+      is_infected: false,
+      viruses: [],
+    }
+  }
+  let chunk = new Buffer.from(upload.data);
+
   const fileStream = new Readable();
-  fileStream.push(upload.data);
-  fileStream.push(null);
-  const result = await av.scanStream(fileStream);
+  fileStream._read = () => {
+    fileStream.push(chunk);
+    chunk = null;
+  }
+
+  const tcpStream = av.passthrough();
+  fileStream.pipe(tcpStream);
+
+  const result = await new Promise(resolve => tcpStream.on("scan-complete", resolve).on("error", (error) => {
+    throw new Error(error);
+  }).on("timeout", (error) => {
+    throw new Error(error);
+  }));
+
   return {
     name: upload.name,
     is_infected: result.isInfected,
